ba9e68c4f1f94f768d56f5e6c0b64d539af28cfd
Reshait <reshait@outlook.com>
2020-08-15 18:51:21 +0200

    functions slpits in files -printers and generators-

--- Planificador1/planificadorMensual1.py
+++ Planificador1/planificadorMensual1.py
@@ -4,13 +4,12 @@ import sys
 
 diaSemana = ['L', 'M', 'X', 'J', 'V', 'S', 'D']
 mesNombre = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio','Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
-mesNum = [1,2,3,4,5,6,7,8,9,10,11,12]
 
 
 def imprimirPlanificadorMensual(numeroMes, numeroAno):
     nombreArchivo = str(numeroMes) + "_" + mesNombre[numeroMes-1] + "_" + str(numeroAno) + ".txt"
     original_stdout = sys.stdout
-    with open(nombreArchivo, 'w') as f:
+    with open("resultados/" + nombreArchivo, 'w') as f:
         sys.stdout = f
         print mesNombre[numeroMes-1] + " de " + str(numeroAno)
         print "--------------------------------------\n\n\n"
@@ -29,8 +28,9 @@ def generarPlanificadorMensual(numeroMes, numeroAno):
             posicionDelDiaDeLaSemana = -1
         posicionDelDiaDeLaSemana += 1
 
+
 print("Introduzca el año que quiere generar -ej: 2020-")
 ano = input()
-for mes in mesNum:
+for mes in range(1,13):
     imprimirPlanificadorMensual(mes, ano)
                                       
