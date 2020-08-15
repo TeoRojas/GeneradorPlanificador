c157ce3cf6eaf264480ef78e7c83945d6458ffd3
Reshait <reshait@outlook.com>
2020-08-15 18:25:31 +0200

    functions slpits in files -printers and generators-

--- planificadorMensual2.py
+++ planificadorMensual2.py
@@ -1,41 +1,10 @@
 # -*- coding: utf-8 -*-
-import calendar
-from calendar import monthrange
-import sys
-import numpy as np
-from funcionesImpresion import *
-import math
+from funcionesgeneracion import *
 
 ano = 2019
 mes = 9
-diaSemana = ['L', 'M', 'X', 'J', 'V', 'S', 'D']
-nSemanasDelMes = len(calendar.monthcalendar(ano,mes))
-
-def cantidadSemanasDelMes(mes, ano):
-    #print "número de mes: " + str(mes)
-    #print "número de semanas de ese mes: " + str(len(calendar.monthcalendar(ano,mes)))
-    return len(calendar.monthcalendar(ano,mes))
-
-def generaMatrizMensual(mes, ano):
-    posicionDelCursor = 0
-    valorDeDia = 1
-    cantidadSemanas = cantidadSemanasDelMes(mes,ano)
-    posicionDelDiaDeLaSemana = monthrange(ano, mes)[0]
-    cantidadDiasDelMes = monthrange(ano, mes)[1]     
-    a = np.zeros((7, cantidadSemanas))
-
-    for j in range(cantidadSemanas):
-        for i in range(7):
-            if (posicionDelCursor >= posicionDelDiaDeLaSemana and valorDeDia <= cantidadDiasDelMes):
-                a[i,j] = valorDeDia
-                valorDeDia += 1
-            else:
-                a[i,j] = 0
-            posicionDelCursor += 1
-    return a
 
 matriz = generaMatrizMensual(mes, ano)
-
 
 imprimePaginaIzq(matriz, mes)
 imprimePaginaDch(matriz, mes)
