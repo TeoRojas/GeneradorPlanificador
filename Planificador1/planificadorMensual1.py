d72300f9b9375d8ba525b3de2812e2b49c755064
TeoRojas <teofilo.rojas.mata@gmail.com>
2020-08-18 12:23:16 +0200

    all automatic

--- Planificador1/planificadorMensual1.py
+++ Planificador1/planificadorMensual1.py
@@ -4,33 +4,98 @@ import sys
 
 diaSemana = ['L', 'M', 'X', 'J', 'V', 'S', 'D']
 mesNombre = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio','Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
-
+esPagIzq = True
 
 def imprimirPlanificadorMensual(numeroMes, numeroAno):
-    nombreArchivo = str(numeroMes) + "_" + mesNombre[numeroMes-1] + "_" + str(numeroAno) + ".txt"
+    if(mes < 10):
+        nombreArchivo = '0' 
+    else:
+        nombreArchivo = ''
+    nombreArchivo += str(numeroMes+1) + "_" + mesNombre[numeroMes] + "_" + 'pm1' + ".tex"
+
     original_stdout = sys.stdout
-    with open("resultados/" + nombreArchivo, 'w') as f:
+    with open("../Resultados/" + nombreArchivo, 'w') as f:
         sys.stdout = f
-        print mesNombre[numeroMes-1] + " de " + str(numeroAno)
-        print "--------------------------------------\n\n\n"
-        generarPlanificadorMensual(numeroMes, numeroAno)
+        #generarPlanificadorMensual(numeroMes, numeroAno)
+        imprimePagIzq(mesNombre[numeroMes], esPagIzq)
+        imprimePagDrcha(numeroMes, numeroAno, esPagIzq)
         sys.stdout = original_stdout 
 
 def generarPlanificadorMensual(numeroMes, numeroAno):
-    posicionDelDiaDeLaSemana = monthrange(numeroAno, numeroMes)[0]
-    cantidadDiasDelMes = monthrange(numeroAno, numeroMes)[1]    
+    posicionDelDiaDeLaSemana = monthrange(numeroAno, numeroMes+1)[0]
+    cantidadDiasDelMes = monthrange(numeroAno, numeroMes+1)[1]    
     for numeroDia in range(cantidadDiasDelMes):
         dia = diaSemana[posicionDelDiaDeLaSemana]
         print ( '\t\t' + dia + '_{' + str(numeroDia+1) + '}' + ' \dotfill' + "\\"+ "\\")
         if(dia == 'D'):
-            print '\t\t\hline'
+            print '\t\t\\hline'
         if (posicionDelDiaDeLaSemana == 6):
             posicionDelDiaDeLaSemana = -1
         posicionDelDiaDeLaSemana += 1
 
+    print '\n\t\t\\bottomrule\n'
+
+
+def imprimeCabecera(mesNombre, esPagIzq):
+    if(esPagIzq):
+        print '{\\raggedleft'
+    else:
+        print '{\\raggedright'
+
+    print '\t\\fontsize{25}{50}\\selectfont'
+
+    if(esPagIzq):
+        print '\t\\textbf{' + mesNombre + '}\\\\'
+    else:
+        print '\t\\textbf{\\NextYear}'
+
+    if(esPagIzq):
+        print '}'
+    else:
+        print '}\\scriptsize{\\textbf{planificador mensual$_1$}}\\\\[11.3pt]' 
+    
+    print '\n\n'
+
+    if(esPagIzq):
+        print '\t\\textbf{\\\\Foco del mes:} \\dotfill'
+    else:
+        print '\t\\noindent\\dotfill'
+
+    print '\t\\renewcommand{\\arraystretch}{1.5}\\scriptsize' 
+    print '\t\t\\begin{longtabu} to \\textwidth { X[l]}'  
+    print '\t\t\\centering \\small{\\textbf{TO-DO}} \\\\'
+    print '\t\t\\toprule'    
+
+def imprimeCuadroYPuntos():
+    print '\t\t\\makebox{$\\square$} \\dotfill\\\\' 
+
+def imprimeCuerpoPagIzq():
+    for linea in range(25):
+        imprimeCuadroYPuntos()
+    print '\n\t\t\\bottomrule\n'
+    print '\t\t\\\\'
+    print '\t\t\\small{\\textbf{PROYECTOS}} \\\\'
+    for linea in range(4):
+        imprimeCuadroYPuntos()
+
+def cierraPag():
+    print '\t\\end{longtabu}\n\n'
+    print '\\clearpage'
+
+def imprimePagIzq(mesNombre, esPagIzq):
+    imprimeCabecera(mesNombre, esPagIzq)
+    imprimeCuerpoPagIzq()
+    cierraPag()
+
+def imprimePagDrcha(numeroMes, numeroAno, esPagIzq):
+    imprimeCabecera(mesNombre[numeroMes], not esPagIzq)
+    generarPlanificadorMensual(numeroMes, numeroAno)  
+    cierraPag()
+
+
 
 print("Introduzca el año que quiere generar -ej: 2020-")
 ano = input()
-for mes in range(1,13):
+for mes in range(12):
     imprimirPlanificadorMensual(mes, ano)
-                                      
+
